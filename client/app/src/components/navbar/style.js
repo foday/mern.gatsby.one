5c7587fad7a11ec7900bbb873adc5bf7fafc4a7e
Sim <foday_s@yahoo.com>
2022-08-10 09:09:45 -0700

    installation and folder structure

---
+++ client/app/src/components/navbar/style.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function style() {
+  return (
+    <div>style</div>
+  )
+}
+
+export default style
---
+++ client/app/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+
+export default function Home() {
+  return <div>
+    <Layout>
+      
+      Hello world
+    </Layout>
+    </div>
+}
